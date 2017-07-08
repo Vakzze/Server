d59acea7e33614cbdc45df5a93ee0e9de225cbd0
Rasmus <fms719@alumni.ku.dk>
2017-07-08 14:55:13 +0200

    adds message from server to client

--- server.c
+++ server.c
@@ -9,12 +9,17 @@
 #include <sys/types.h>
 #include <time.h> 
 
+
+int getLine(char s[], int lim);
+
+
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr; 
     int count = 0;
     char welcomeMassage[100] = "Hello and welcome to my server";
+    char message[1000];
     char sendBuff[1025];
     time_t ticks; 
 
@@ -43,8 +48,34 @@ int main(int argc, char *argv[])
         printf("Nummber of connections: %d\n",count);
         snprintf(sendBuff, sizeof(sendBuff), "%s\n%.24s\r\n", welcomeMassage, ctime(&ticks));
         write(connfd, sendBuff, strlen(sendBuff)); 
+        
+        printf("Type a something to the client:\n");
+        getLine(message,1000);
+        printf("%s", message);
 
         close(connfd);
         sleep(1);
      }
 }
+
+
+int getLine(char s[], int lim)
+{
+    int c, i;
+    for(i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+    {
+        s[i] = c;
+    }
+
+    if(c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+                     
+    return i;
+   
+}
+
+
